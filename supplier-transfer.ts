2063e723e1be0a423b0de683bdc45b4b89763668
aiden <aidenhsy@gmail.com>
2025-03-11 15:41:11 +0800

    updates

--- supplier-transfer.ts
+++ supplier-transfer.ts
@@ -8,27 +8,38 @@ const run = async () => {
   const supplierPerson = await prisma.scm_supplier_person.findMany();
   const sellerAccount = await prisma.scm_seller_account.findMany();
 
-  const supplierMap = supplierPerson.map((person) => ({
-    name: person.name!,
-    mobile: person.phone_number!,
-    password: person.password!,
-    user_type: user_type_enum.SUPPLIER_TONGPEI,
-  }));
+  for (const supplier of supplierPerson) {
+    await prisma.users.create({
+      data: {
+        name: supplier.name!,
+        mobile: supplier.phone_number!,
+        password: supplier.password!,
+        user_type: user_type_enum.SUPPLIER_TONGPEI,
+      },
+    });
+  }
 
-  const sellerAccountMap = sellerAccount.map((seller) => ({
-    name: seller.moniker!,
-    mobile: seller.mobile!,
-    password: seller.password!,
-    user_type: user_type_enum.SUPPLIER_ZHIPEI,
-  }));
+  // const supplierMap = supplierPerson.map((person) => ({
+  //   name: person.name!,
+  //   mobile: person.phone_number!,
+  //   password: person.password!,
+  //   user_type: user_type_enum.SUPPLIER_TONGPEI,
+  // }));
 
-  await prisma.users.createMany({
-    data: supplierMap,
-  });
+  // const sellerAccountMap = sellerAccount.map((seller) => ({
+  //   name: seller.moniker!,
+  //   mobile: seller.mobile!,
+  //   password: seller.password!,
+  //   user_type: user_type_enum.SUPPLIER_ZHIPEI,
+  // }));
 
-  await prisma.users.createMany({
-    data: sellerAccountMap,
-  });
+  // await prisma.users.createMany({
+  //   data: supplierMap,
+  // });
+
+  // await prisma.users.createMany({
+  //   data: sellerAccountMap,
+  // });
 };
 
 run();
