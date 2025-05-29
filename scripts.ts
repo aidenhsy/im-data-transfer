41151a3a29f7f3bd1134b4e6d7678a2cc6805bc6
aiden <aidenhsy@gmail.com>
2025-05-29 15:59:52 +0900

    added city id to supplier good by city

--- scripts.ts
+++ scripts.ts
@@ -5,22 +5,59 @@ const run = async () => {
   const scmClient = new SCMClient();
   const imClient = new IMClient();
 
-  const goods = await imClient.supplier_items.findMany();
-  for (const good of goods) {
-    const scmGoodPrice = await scmClient.scm_good_pricing.findFirst({
-      where: {
-        version: '20250526',
-        goods_id: good.id,
-        client_tier_id: 2,
+  const goods = await scmClient.scm_good_pricing.findMany({
+    where: {
+      version: '20250528',
+      client_tier_id: 2,
+      scm_goods: {
+        status: 1,
       },
-    });
-    if (scmGoodPrice) {
-      await imClient.supplier_items.update({
-        where: {
-          id: good.id,
+    },
+    take: 10,
+    include: {
+      scm_good_units: true,
+      scm_goods: {
+        include: {
+          scm_good_units_scm_goods_base_good_unit_idToscm_good_units: true,
+          scm_stock: true,
         },
+      },
+    },
+  });
+
+  for (const good of goods) {
+    if (
+      good.scm_goods.scm_good_units_scm_goods_base_good_unit_idToscm_good_units
+        ?.name === '克' ||
+      '毫升' ||
+      '个'
+    ) {
+      const unitName =
+        good.scm_goods
+          .scm_good_units_scm_goods_base_good_unit_idToscm_good_units?.name;
+
+      let baseUnitId = 1;
+      if (unitName === '克') {
+        baseUnitId = 1;
+      } else if (unitName === '毫升') {
+        baseUnitId = 6;
+      } else if (unitName === '个') {
+        baseUnitId = 8;
+      }
+
+      await imClient.supplier_items.create({
         data: {
-          supplier_reference_id: scmGoodPrice.external_reference_id,
+          name: good.scm_goods.name,
+          supplier_reference_id: good.external_reference_id,
+          letter_name: good.scm_goods.letter_name,
+          supplier_id: 1,
+          status: good.scm_goods.status,
+          package_unit_name: good.scm_good_units.name,
+          photo_url: good.scm_goods.photo_url,
+          package_unit_to_base_ratio: Number(good.scm_good_units.ratio_to_base),
+          price: good.sale_price,
+          cut_off_time: good.scm_goods.scm_stock?.sold_time?.toString(),
+          base_unit_id: baseUnitId,
         },
       });
     }
