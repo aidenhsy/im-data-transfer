bbf3f646768ea73f62cdf34a16e4b8d4ca2f1263
aiden <aidenhsy@gmail.com>
2025-06-24 22:43:58 +0900

    transfer bug

--- 1-1-transfer-scm-pricing.ts
+++ 1-1-transfer-scm-pricing.ts
@@ -4,6 +4,7 @@ import { PrismaClient as SCMPricing } from './prisma/clients/scm-pricing';
 
 const run = async () => {
   const scm = new SCMProd();
+  const im = new IMProd();
   const scmPricing = new SCMPricing();
 
   const VERSION = '20250624';
@@ -44,21 +45,62 @@ const run = async () => {
       });
     }
 
-    await scmPricing.scm_good_pricing.create({
-      data: {
-        client_tier_id: prod.client_tier_id,
-        profit_margin: prod.profit_margin,
-        sale_price: prod.sale_price,
-        is_active: prod.is_active,
-        city_id: 1,
-        cut_off_time: prod?.cut_off_time ? prod.cut_off_time : '15:00:00',
-        goods_id: prod.goods_id,
-        good_unit_id: prod.good_unit_id,
-        pricing_strategy: prod.pricing_strategy,
-        version: VERSION,
-        locked_after: LOCKED_AFTER_DATE,
+    const planIds = await im.scm_supply_plan_scm_goods.findMany({
+      where: {
+        reference_id: prod.id,
+      },
+      distinct: ['supply_plan_id'],
+      select: {
+        supply_plan_id: true,
       },
     });
+    const shops = await im.scm_shop.findMany({
+      where: {
+        supply_plan_id: {
+          in: planIds.map((i) => i.supply_plan_id).filter((i) => i !== null),
+        },
+      },
+      select: {
+        city_id: true,
+      },
+    });
+
+    if (shops.length === 0) {
+      await scmPricing.scm_good_pricing.create({
+        data: {
+          client_tier_id: prod.client_tier_id,
+          profit_margin: prod.profit_margin,
+          sale_price: prod.sale_price,
+          is_active: prod.is_active,
+          city_id: 1,
+          cut_off_time: prod?.cut_off_time ? prod.cut_off_time : '15:00:00',
+          goods_id: prod.goods_id,
+          good_unit_id: prod.good_unit_id,
+          pricing_strategy: prod.pricing_strategy,
+          version: VERSION,
+          locked_after: LOCKED_AFTER_DATE,
+        },
+      });
+      continue;
+    }
+
+    for (const shop of shops) {
+      await scmPricing.scm_good_pricing.create({
+        data: {
+          client_tier_id: prod.client_tier_id,
+          profit_margin: prod.profit_margin,
+          sale_price: prod.sale_price,
+          is_active: prod.is_active,
+          city_id: shop.city_id,
+          cut_off_time: prod?.cut_off_time ? prod.cut_off_time : '15:00:00',
+          goods_id: prod.goods_id,
+          good_unit_id: prod.good_unit_id,
+          pricing_strategy: prod.pricing_strategy,
+          version: VERSION,
+          locked_after: LOCKED_AFTER_DATE,
+        },
+      });
+    }
   }
 
   const devPricings = await scmPricing.scm_good_pricing.count({
