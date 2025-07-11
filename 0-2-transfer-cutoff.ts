b10ff020195ab899b60fffcb8f939bea8d17714e
Aiden Yang <aidenhsy@gmail.com>
2025-07-11 11:00:20 +0900

    updates

---
+++ 0-2-transfer-cutoff.ts
@@ -0,0 +1,39 @@
+import { PrismaClient as IMProd } from './prisma/clients/im-prod';
+import { PrismaClient as SCMProd } from './prisma/clients/scm-prod';
+
+const run = async () => {
+  const scm = new SCMProd();
+  const im = new IMProd();
+
+  const goods = await im.scm_supply_plan_scm_goods.findMany({
+    where: {
+      reference_id: {
+        not: null,
+      },
+    },
+  });
+
+  for (const good of goods) {
+    const goodPricing = await scm.scm_good_pricing.findFirst({
+      where: {
+        id: good.reference_id!,
+      },
+    });
+
+    if (!goodPricing) {
+      console.log(`Good pricing not found for ${good.reference_id}`);
+      continue;
+    }
+
+    await scm.scm_good_pricing.update({
+      where: {
+        id: goodPricing.id,
+      },
+      data: {
+        cut_off_time: good.sold_time,
+      },
+    });
+  }
+};
+
+run();
