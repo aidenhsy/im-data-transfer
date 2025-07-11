e5bcd151b078d04fbb4e6724c96ec900205ff053
Aiden Yang <aidenhsy@gmail.com>
2025-07-11 16:15:40 +0900

    updates

--- final-1.ts
+++ final-1.ts
@@ -11,14 +11,14 @@ const run = async () => {
   const scm = new Scm();
   const im = new IM();
   const scmPricing = new ScmPricing();
-  const imProcurement = new IMProcurement();
+  // const imProcurement = new IMProcurement();
 
-  const VERSION = '20250711';
-  const LOCKED_AFTER_DATE = new Date('2025-07-11T03:30:00.000Z');
+  const VERSION = '20250712';
+  const LOCKED_AFTER_DATE = new Date('2025-07-12T03:30:00.000Z');
 
-  await imProcurement.plan_item_supplier_good.deleteMany();
-  await imProcurement.supply_plan_items.deleteMany();
-  await imProcurement.supplier_items.deleteMany();
+  // await imProcurement.plan_item_supplier_good.deleteMany();
+  // await imProcurement.supply_plan_items.deleteMany();
+  // await imProcurement.supplier_items.deleteMany();
   await scmPricing.scm_good_pricing.deleteMany({
     where: {
       version: VERSION,
@@ -47,14 +47,14 @@ const run = async () => {
         },
       }
     );
-    const brand = await imProcurement.scm_shop_brand.findFirst({
-      where: {
-        id: shop.brand_id,
-      },
-      select: {
-        supply_plan_id: true,
-      },
-    });
+    // const brand = await imProcurement.scm_shop_brand.findFirst({
+    //   where: {
+    //     id: shop.brand_id,
+    //   },
+    //   select: {
+    //     supply_plan_id: true,
+    //   },
+    // });
 
     for (const category of data.data) {
       for (const good of category.goods) {
@@ -133,59 +133,59 @@ const run = async () => {
           },
         });
 
-        const supplierItem = await imProcurement.supplier_items.upsert({
-          where: {
-            supplier_reference_id: pricing.external_reference_id!,
-          },
-          update: {},
-          create: {
-            name: good.goodsName,
-            status: 1,
-            create_time: new Date(),
-            update_time: new Date(),
-            letter_name: good.letterName,
-            supplier_id: 1,
-            photo_url: good.filePath,
-            price: pricing.sale_price,
-            supplier_reference_id: pricing.external_reference_id!,
-            cut_off_time: pricing.cut_off_time,
-            package_unit_to_base_ratio: good.ratio,
-            package_unit_name: good.saleUnit,
-            base_unit_id: pricing.scm_goods.standard_base_unit,
-            city_id: shop.city_id,
-            weighing: good.weighing,
-            tier_id: shop.client_tier_id!,
-          },
-        });
+        // const supplierItem = await imProcurement.supplier_items.upsert({
+        //   where: {
+        //     supplier_reference_id: pricing.external_reference_id!,
+        //   },
+        //   update: {},
+        //   create: {
+        //     name: good.goodsName,
+        //     status: 1,
+        //     create_time: new Date(),
+        //     update_time: new Date(),
+        //     letter_name: good.letterName,
+        //     supplier_id: 1,
+        //     photo_url: good.filePath,
+        //     price: pricing.sale_price,
+        //     supplier_reference_id: pricing.external_reference_id!,
+        //     cut_off_time: pricing.cut_off_time,
+        //     package_unit_to_base_ratio: good.ratio,
+        //     package_unit_name: good.saleUnit,
+        //     base_unit_id: pricing.scm_goods.standard_base_unit,
+        //     city_id: shop.city_id,
+        //     weighing: good.weighing,
+        //     tier_id: shop.client_tier_id!,
+        //   },
+        // });
 
-        const supplyPlanItem = await imProcurement.supply_plan_items.upsert({
-          where: {
-            supply_plan_id_item_id: {
-              supply_plan_id: brand!.supply_plan_id!,
-              item_id: pricing.goods_id,
-            },
-          },
-          update: {},
-          create: {
-            supply_plan_id: brand!.supply_plan_id!,
-            item_id: pricing.goods_id,
-          },
-        });
+        // const supplyPlanItem = await imProcurement.supply_plan_items.upsert({
+        //   where: {
+        //     supply_plan_id_item_id: {
+        //       supply_plan_id: brand!.supply_plan_id!,
+        //       item_id: pricing.goods_id,
+        //     },
+        //   },
+        //   update: {},
+        //   create: {
+        //     supply_plan_id: brand!.supply_plan_id!,
+        //     item_id: pricing.goods_id,
+        //   },
+        // });
 
-        await imProcurement.plan_item_supplier_good.upsert({
-          where: {
-            plan_item_id_shop_id: {
-              plan_item_id: supplyPlanItem.id,
-              shop_id: shop.id,
-            },
-          },
-          update: {},
-          create: {
-            plan_item_id: supplyPlanItem.id,
-            supplier_item_id: supplierItem.id,
-            shop_id: shop.id,
-          },
-        });
+        // await imProcurement.plan_item_supplier_good.upsert({
+        //   where: {
+        //     plan_item_id_shop_id: {
+        //       plan_item_id: supplyPlanItem.id,
+        //       shop_id: shop.id,
+        //     },
+        //   },
+        //   update: {},
+        //   create: {
+        //     plan_item_id: supplyPlanItem.id,
+        //     supplier_item_id: supplierItem.id,
+        //     shop_id: shop.id,
+        //   },
+        // });
       }
     }
 
