faf82df0721e50049cc35eae838ed5dc7f906f7d
Aiden Yang <aidenhsy@gmail.com>
2025-07-14 18:15:07 +0900

    bug fix

---
+++ aasyncnames.ts
@@ -0,0 +1,45 @@
+import { PrismaClient as IM } from './prisma/clients/im-prod';
+import { PrismaClient as IMProcurement } from './prisma/clients/im-procurement-prod';
+import { PrismaClient as Scm } from './prisma/clients/scm-prod';
+import { PrismaClient as ScmPricing } from './prisma/clients/scm-pricing-prod';
+
+const run = async () => {
+  const im = new IM();
+  const imProcurement = new IMProcurement();
+  const scm = new Scm();
+  const scmPricing = new ScmPricing();
+
+  const supplierGoods = await imProcurement.supplier_items.findMany();
+  const length = supplierGoods.length;
+
+  let i = 0;
+
+  for (const supplierGood of supplierGoods) {
+    i++;
+    console.log(`${i}/${length}`);
+    const pricing = await scmPricing.scm_good_pricing.findFirst({
+      where: {
+        external_reference_id: supplierGood.supplier_reference_id,
+      },
+      select: {
+        scm_goods: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    if (!pricing) {
+      console.log(supplierGood.supplier_reference_id);
+      continue;
+    }
+    await imProcurement.supplier_items.update({
+      where: { id: supplierGood.id },
+      data: {
+        name: pricing?.scm_goods?.name,
+      },
+    });
+  }
+};
+
+run();
