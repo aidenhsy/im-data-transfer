79484ac837de25402ed99e11ab77dc736fa7899a
Aiden Yang <aidenhsy@gmail.com>
2025-07-14 10:57:22 +0900

    updates

--- 0-0-sync-shops.ts
+++ 0-0-sync-shops.ts
@@ -1,23 +1,47 @@
 import { PrismaClient as IM } from './prisma/clients/im-prod';
 import { PrismaClient as IMProcurement } from './prisma/clients/im-procurement-prod';
+import { PrismaClient as Scm } from './prisma/clients/scm-prod';
+import { PrismaClient as ScmPricing } from './prisma/clients/scm-pricing-prod';
 
 const run = async () => {
   const im = new IM();
+  const scm = new Scm();
   const imProcurement = new IMProcurement();
+  const scmPricing = new ScmPricing();
 
-  const shops = await im.scm_shop.findMany();
+  const shops = await scm.scm_shop.findMany();
+  const businessDistricts = await scm.scm_business_district.findMany();
+
+  for (const businessDistrict of businessDistricts) {
+    await scmPricing.scm_business_district.upsert({
+      where: {
+        id: businessDistrict.id,
+      },
+      update: {
+        ...businessDistrict,
+      },
+      create: {
+        ...businessDistrict,
+      },
+    });
+  }
 
   for (const shop of shops) {
+    await scmPricing.scm_shop.upsert({
+      where: {
+        id: shop.id,
+      },
+      update: {},
+      create: {
+        ...shop,
+      },
+    });
+
     await imProcurement.scm_shop.upsert({
       where: {
         id: shop.id,
       },
-      update: {
-        status: shop.status,
-        city_id: shop.city_id,
-        client_tier_id: shop.client_tier_id,
-        automatic_receiving: shop.automatic_receiving,
-      },
+      update: {},
       create: {
         id: shop.id,
         shop_name: shop.shop_name,
@@ -33,7 +57,7 @@ const run = async () => {
         client_tier_id: shop.client_tier_id,
         automatic_receiving: shop.automatic_receiving,
         sales_plan_id: shop.sales_plan_id,
-        big_org_id: shop.big_org_id,
+        big_org_id: shop.organization_id,
       },
     });
   }
