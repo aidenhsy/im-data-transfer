3e70f886342b9dcf4fd51cb2b99635520a888eaf
Aiden Yang <aidenhsy@gmail.com>
2025-07-15 09:59:05 +0900

    changes

--- check-prices.ts
+++ check-prices.ts
@@ -23,6 +23,29 @@ const run = async () => {
         `${good.id} ${good.name} ${good.price} ${pricing?.price} not equal`
       );
     }
+
+    const pricings = await scmPricing.scm_good_pricing.findMany({
+      where: {
+        goods_id: good.id,
+      },
+    });
+
+    for (const item of pricings) {
+      if (item.pricing_strategy === 'margin') {
+        const correctPrice =
+          Math.round(
+            Number(good.price) * (1 + Number(item.profit_margin) / 100) * 100
+          ) / 100;
+
+        const currentPrice = Math.round(Number(item.sale_price) * 100) / 100;
+
+        if (Number(correctPrice) !== Number(currentPrice)) {
+          console.log(
+            `${good.id} ${good.name} ${correctPrice} ${currentPrice} not equal`
+          );
+        }
+      }
+    }
   }
 };
 
