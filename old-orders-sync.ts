da8e13f0052b43123053b278842bde4b5188a826
Aiden Yang <aidenhsy@gmail.com>
2025-07-16 18:55:38 +0900

    updates

--- old-orders-sync.ts
+++ old-orders-sync.ts
@@ -1,39 +1,65 @@
 import { PrismaClient as IMProcurement } from './prisma/clients/im-procurement-prod';
 import { PrismaClient as IMProcurementDev } from './prisma/clients/im-procurement-dev';
 import { PrismaClient as ScmOrder } from './prisma/clients/scm-order-prod';
+import { PrismaClient as Scm } from './prisma/clients/scm-prod';
+import { PrismaClient as ScmPricing } from './prisma/clients/scm-pricing-prod';
 
 const run = async () => {
   const imProcurementDev = new IMProcurementDev();
   const imProcurementProd = new IMProcurement();
+  const scmProd = new Scm();
   const scmOrderProd = new ScmOrder();
+  const scmPricingProd = new ScmPricing();
 
-  const oldRecords = await imProcurementDev.old_records.findMany();
+  const shops = await imProcurementProd.scm_shop.findMany();
+  const dates = [
+    '2025-07-01',
+    '2025-07-02',
+    '2025-07-03',
+    '2025-07-04',
+    '2025-07-05',
+    '2025-07-06',
+    '2025-07-07',
+    '2025-07-08',
+    '2025-07-09',
+    '2025-07-10',
+    '2025-07-11',
+    '2025-07-12',
+    '2025-07-13',
+    '2025-07-14',
+  ];
 
-  const length = oldRecords.length;
-  let num = 0;
+  for (const shop of shops) {
+    for (const date of dates) {
+      const orderItems = await imProcurementDev.old_records.findMany({
+        where: {
+          shop_id: shop.id.toString(),
+          delivery_date: date,
+        },
+      });
+      for (const item of orderItems) {
+        const orderDetailItem = await scmProd.scm_order_details.findFirst({
+          where: {
+            id: Number(item.id),
+          },
+          include: {
+            scm_order: true,
+          },
+        });
+        const goodPricing = await scmPricingProd.scm_good_pricing.findFirst({
+          where: {
+            goods_id: Number(orderDetailItem?.goods_id),
+          },
+        });
+        console.log(goodPricing);
 
-  for (const record of oldRecords) {
-    num++;
-    console.log(`${num}/${length}`);
-    const shop = await imProcurementProd.scm_shop.findFirst({
-      where: {
-        shop_name: record.store_name!,
-      },
-    });
-
-    if (!shop) {
-      console.log(`Shop ${record.store_name} not found`);
-      continue;
+        // console.log(
+        //   orderDetailItem?.reference_id,
+        //   orderDetailItem?.reference_order_id
+        // );
+      }
+      break;
     }
-
-    await imProcurementDev.old_records.update({
-      where: {
-        id: record.id,
-      },
-      data: {
-        shop_id: shop.id.toString(),
-      },
-    });
   }
 };
 
