cd59636c45c5a24e57df7df0aeee619e308d00dc
Aiden Yang <aidenhsy@gmail.com>
2025-07-17 12:46:42 +0900

    updates

--- check-prices.ts
+++ check-prices.ts
@@ -31,7 +31,7 @@ const run = async () => {
     const pricings = await scmPricing.scm_good_pricing.findMany({
       where: {
         goods_id: good.id,
-        version: '20250716',
+        version: '20250717',
       },
     });
 
