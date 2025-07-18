1f43ec9b0d50c5c747a2732f085ae0600bb1fa20
Aiden Yang <aidenhsy@gmail.com>
2025-07-19 08:37:54 +0900

    updated time

--- cleaning/check-city.ts
+++ cleaning/check-city.ts
@@ -8,46 +8,38 @@ const run = async () => {
   const imProcurementDB = new ImProcurementDB();
   const scmPricingDB = new ScmPricingDB();
 
-  const orderDetails = await imProcurementDB.supplier_order_details.findMany({
-    select: {
-      supplier_reference_id: true,
-      supplier_orders: {
-        select: {
-          shop_id: true,
+  const planSupplierItems =
+    await imProcurementDB.plan_item_supplier_good.findMany({
+      select: {
+        shop_id: true,
+        supplier_items: {
+          select: {
+            supplier_reference_id: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  const length = orderDetails.length;
-  let count = 0;
-
-  for (const detail of orderDetails) {
-    count++;
-    if (count % 10000 === 0) {
-      console.log(`${count}/${length}`);
-    }
-
+  for (const item of planSupplierItems) {
     const shop = await scmPricingDB.scm_shop.findFirst({
       where: {
-        id: detail.supplier_orders.shop_id,
+        id: Number(item.shop_id),
       },
     });
 
-    const matchedPricings = await scmPricingDB.scm_good_pricing.findFirst({
-      where: {
-        external_reference_id: detail.supplier_reference_id,
-      },
-    });
-    if (Number(matchedPricings?.city_id) !== Number(shop?.city_id)) {
-      console.log(
-        detail.supplier_reference_id,
-        shop?.city_id,
-        matchedPricings?.city_id,
-        'city mismatch'
-      );
-      continue;
+    // Just console log 3 got from supplier_reference_id
+    const supplierReferenceId = item.supplier_items?.supplier_reference_id;
+    if (supplierReferenceId) {
+      const parts = supplierReferenceId.split('-');
+      console.log(parts[3]);
+      if (Number(parts[3]) !== Number(shop?.city_id)) {
+        console.log(
+          item.supplier_items?.supplier_reference_id,
+          'city mismatch'
+        );
+      }
     }
+    break;
   }
 };
 
