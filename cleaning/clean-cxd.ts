dd16be40dc23caf5a90b3278e22ef1d2d7dc7288
Aiden Yang <aidenhsy@gmail.com>
2025-07-18 13:56:43 +0900

    updated time

---
+++ cleaning/clean-cxd.ts
@@ -0,0 +1,27 @@
+import { PrismaClient as ImProcurementDB } from '../prisma/clients/im-procurement-prod';
+import { PrismaClient as ScmPricingDB } from '../prisma/clients/scm-pricing-prod';
+
+const run = async () => {
+  const imProcurementDB = new ImProcurementDB();
+  const scmPricingDB = new ScmPricingDB();
+
+  const pricings = await scmPricingDB.scm_good_pricing.findMany({
+    where: {
+      version: '20250718',
+    },
+  });
+
+  for (const pricing of pricings) {
+    const existSupplierItem = await imProcurementDB.supplier_items.findFirst({
+      where: {
+        supplier_reference_id: pricing.external_reference_id,
+      },
+    });
+
+    if (!existSupplierItem) {
+      console.log(pricing.external_reference_id);
+    }
+  }
+};
+
+run();
