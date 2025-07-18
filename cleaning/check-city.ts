a7f1f1ad990c4d659ce3ea15d22f50b84379853e
Aiden Yang <aidenhsy@gmail.com>
2025-07-19 08:02:34 +0900

    updated time

--- cleaning/check-city.ts
+++ cleaning/check-city.ts
@@ -8,7 +8,16 @@ const run = async () => {
   const imProcurementDB = new ImProcurementDB();
   const scmPricingDB = new ScmPricingDB();
 
-  const orderDetails = await imProcurementDB.supplier_order_details.findMany();
+  const orderDetails = await imProcurementDB.supplier_order_details.findMany({
+    select: {
+      supplier_reference_id: true,
+      supplier_orders: {
+        select: {
+          shop_id: true,
+        },
+      },
+    },
+  });
 
   const length = orderDetails.length;
   let count = 0;
@@ -17,6 +26,22 @@ const run = async () => {
     count++;
     if (count % 10000 === 0) {
       console.log(`${count}/${length}`);
+    }
+
+    const shop = await scmPricingDB.scm_shop.findFirst({
+      where: {
+        id: detail.supplier_orders.shop_id,
+      },
+    });
+
+    const matchedPricings = await scmPricingDB.scm_good_pricing.findFirst({
+      where: {
+        external_reference_id: detail.supplier_reference_id,
+      },
+    });
+    if (matchedPricings?.city_id !== shop?.city_id) {
+      console.log(detail.supplier_reference_id, 'city mismatch');
+      continue;
     }
   }
 };
