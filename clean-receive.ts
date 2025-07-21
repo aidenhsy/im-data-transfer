5bf80239fb07a82a36f261d2498e81be8837782e
Aiden Yang <aidenhsy@gmail.com>
2025-07-21 17:40:37 +0900

    更新了 process.env使用方法

--- clean-receive.ts
+++ clean-receive.ts
@@ -1,8 +1,6 @@
 import { PrismaClient as IMProcurement } from './prisma/clients/im-procurement-prod';
-import { PrismaClient as IMProcurementDev } from './prisma/clients/im-procurement-dev';
 import { PrismaClient as ScmOrder } from './prisma/clients/scm-order-prod';
 import { PrismaClient as Scm } from './prisma/clients/scm-prod';
-import { PrismaClient as ScmPricing } from './prisma/clients/scm-pricing-prod';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
