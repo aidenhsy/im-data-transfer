9a3e77c94cab95b49b6c94569ca03fe5ead4a885
Aiden Yang <aidenhsy@gmail.com>
2025-07-21 09:25:05 +0900

    bug fix

--- fill-in-old-prices.ts
+++ fill-in-old-prices.ts
@@ -1,9 +1,6 @@
-import { PrismaClient as IMProcurement } from './prisma/clients/im-procurement-prod';
-import { PrismaClient as IMBasicData } from './prisma/clients/im-basic-data-prod';
 import { PrismaClient as ScmPricing } from './prisma/clients/scm-pricing-prod';
 
 const run = async () => {
-  const imProcurement = new IMProcurement();
   const scmPricing = new ScmPricing();
 
   const pricings = await scmPricing.scm_good_pricing.findMany();
