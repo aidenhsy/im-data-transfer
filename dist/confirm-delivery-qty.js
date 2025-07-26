18881d934c46864ee59581b950f25462b8152b87
Aiden Yang <aidenhsy@gmail.com>
2025-07-26 21:20:22 +0900

    updates

--- dist/confirm-delivery-qty.js
+++ dist/confirm-delivery-qty.js
@@ -7,25 +7,43 @@ const run = async () => {
     const imProcurement = new im_procurement_prod_1.PrismaClient();
     const scmOrder = new scm_order_prod_1.PrismaClient();
     const scm = new scm_prod_1.PrismaClient();
-    const orders = await imProcurement.supplier_orders.findMany({
+    const orderDetails = await imProcurement.supplier_order_details.findMany({
         where: {
-            status: 20,
-        },
-        include: {
-            supplier_order_details: true,
+            actual_delivery_qty: null,
+            supplier_orders: {
+                status: 20,
+            },
         },
     });
-    for (const order of orders) {
-        let missMatch = 0;
-        for (const detail of order.supplier_order_details) {
-            if (Number(detail.confirm_delivery_qty) !==
-                Number(detail.actual_delivery_qty)) {
-                missMatch++;
-            }
+    for (const detail of orderDetails) {
+        const scmDetail = await scmOrder.procurement_order_details.findFirst({
+            where: {
+                reference_id: detail.supplier_reference_id,
+                procurement_orders: {
+                    client_order_id: detail.order_id,
+                },
+            },
+        });
+        if (!scmDetail) {
+            console.log(detail.id);
+            continue;
         }
-        if (missMatch === 0) {
-            console.log(order.id);
-        }
+        await imProcurement.supplier_order_details.update({
+            where: {
+                id: detail.id,
+            },
+            data: {
+                confirm_delivery_qty: scmDetail.deliver_qty,
+            },
+        });
+        await scmOrder.procurement_order_details.update({
+            where: {
+                id: scmDetail.id,
+            },
+            data: {
+                customer_receive_qty: scmDetail.deliver_qty,
+            },
+        });
     }
 };
 run();
