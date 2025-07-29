9bfee8f841de5b2d30272010fa2e270b53b614c9
Aiden Yang <aidenhsy@gmail.com>
2025-07-30 06:41:43 +0900

    updates

--- dist/add-supplier-missing.js
+++ dist/add-supplier-missing.js
@@ -13,12 +13,16 @@ const run = async () => {
         },
     });
     for (const item of missingItems) {
-        const sectionId = item.supplier_reference_id.split('-').slice(2).join('-');
+        const sectionId = item.supplier_reference_id.split('-').slice(1).join('-');
         const pricingItem = await pricing.scm_good_pricing.findFirst({
             where: {
                 external_reference_id: {
                     endsWith: sectionId,
                 },
+            },
+            include: {
+                scm_goods: true,
+                scm_good_units: true,
             },
             orderBy: {
                 created_at: 'desc',
@@ -28,6 +32,30 @@ const run = async () => {
             console.log(item.supplier_reference_id, 'not found!');
             continue;
         }
+        console.log(sectionId);
+        await procurement.supplier_items.upsert({
+            where: {
+                supplier_reference_id: `20250729-${sectionId}`,
+            },
+            update: {},
+            create: {
+                name: pricingItem.scm_goods.name,
+                status: 0,
+                letter_name: pricingItem.scm_goods.letter_name,
+                supplier_id: 1,
+                photo_url: pricingItem.scm_goods.photo_url,
+                price: pricingItem.sale_price,
+                supplier_reference_id: `20250729-${sectionId}`,
+                cut_off_time: pricingItem.cut_off_time,
+                package_unit_to_base_ratio: Number(pricingItem.scm_good_units.ratio_to_base),
+                package_unit_name: pricingItem.scm_good_units.name,
+                base_unit_id: pricingItem.scm_goods.standard_base_unit,
+                city_id: pricingItem.city_id,
+                weighing: pricingItem.scm_goods.weighing,
+                tier_id: pricingItem.client_tier_id,
+            },
+        });
     }
+    console.log('done');
 };
 run();
