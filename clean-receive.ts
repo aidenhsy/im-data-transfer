9d00d50a855d0a9a3988d777b14098daeecc40d0
aiden <aidenhsy@gmail.com>
2025-08-18 12:42:42 +0900

    updates

--- clean-receive.ts
+++ clean-receive.ts
@@ -23,7 +23,7 @@ const run = async () => {
       orderBy: {
         created_at: 'desc',
       },
-      take: 1000,
+      take: 10000,
     });
 
   for (const imDetail of imProcurementDetails) {
