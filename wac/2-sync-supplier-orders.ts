6ef761cafeca332e8342ad5941b71020e77007c2
Aiden Yang <aidenhsy@gmail.com>
2025-08-21 14:06:50 +0900

    updates

--- wac/2-sync-supplier-orders.ts
+++ wac/2-sync-supplier-orders.ts
@@ -106,4 +106,51 @@ const runOrders = async () => {
   }
 };
 
-runOrders();
+// runOrders();
+
+const runJulyCount = async () => {
+  const inventories = await imInventory.inventory_count.findMany({
+    distinct: ['shop_id'],
+    orderBy: {
+      created_at: 'asc',
+    },
+    where: {
+      created_at: {
+        gte: new Date('2025-07-29'),
+        lte: new Date('2025-08-03'),
+      },
+    },
+    include: {
+      inventory_count_details: {
+        include: {
+          supplier_items: true,
+        },
+      },
+    },
+  });
+
+  for (const inventory of inventories) {
+    for (const inventoryDetail of inventory.inventory_count_details) {
+      await imInventory.shop_item_weighted_price.create({
+        data: {
+          shop_id: Number(inventory.shop_id),
+          supplier_item_id: inventoryDetail.supplier_items.id,
+          total_qty: inventoryDetail.count_qty,
+          total_value:
+            Number(inventoryDetail.count_qty) *
+            Number(inventoryDetail.supplier_items.price),
+          source_id: inventory.id,
+          source_detail_id: inventoryDetail.id,
+          type: 'stock_count',
+          status: 1,
+          created_at: inventory.created_at!,
+          order_to_base_factor: Number(
+            inventoryDetail.supplier_items.package_unit_to_base_ratio
+          ),
+        },
+      });
+    }
+  }
+};
+
+runJulyCount();
