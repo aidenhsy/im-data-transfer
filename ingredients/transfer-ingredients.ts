a998c998b0a5a22f7798ec9c303529d70a408387
aiden <aidenhsy@gmail.com>
2025-08-22 09:43:48 +0900

    updates

---
+++ ingredients/transfer-ingredients.ts
@@ -0,0 +1,35 @@
+import { PrismaClient as ImBasicData } from '../prisma/clients/im-basic-data-prod';
+import { PrismaClient as ImProd } from '../prisma/clients/im-prod';
+
+const run = async () => {
+  const imBasicData = new ImBasicData();
+  const imProd = new ImProd();
+
+  const ingredients = await imProd.st_ingredient.findMany();
+
+  for (const ingredient of ingredients) {
+    const genericItem = await imBasicData.generic_items.findUnique({
+      where: {
+        id: ingredient.goods_id!,
+      },
+    });
+
+    if (!genericItem) {
+      console.log(`Generic item not found for ingredient ${ingredient.id}`);
+      continue;
+    }
+
+    await imBasicData.ingredient.create({
+      data: {
+        id: ingredient.id.toString(),
+        food_item_id: ingredient.food_item_id!.toString(),
+        generic_item_id: ingredient.goods_id!,
+        qty: ingredient.qty,
+        base_unit_id: genericItem.base_unit_id,
+        yield_pct: ingredient.yield_pct,
+      },
+    });
+  }
+};
+
+run();
