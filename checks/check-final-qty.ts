ee7f1fa20576f6bb92e6af73a016b516932d0aa6
aiden <aidenhsy@gmail.com>
2025-08-28 15:43:28 +0900

    updates

---
+++ checks/check-final-qty.ts
@@ -0,0 +1,60 @@
+import { DatabaseService } from '../database';
+
+const run = async () => {
+  const database = new DatabaseService();
+
+  const details =
+    await database.imProcurementProd.supplier_order_details.findMany({
+      where: {
+        supplier_orders: {
+          receive_time: {
+            gte: new Date('2025-08-01T00:00:00.000Z'),
+          },
+        },
+      },
+      orderBy: {
+        created_at: 'desc',
+      },
+    });
+
+  console.log(details.length);
+
+  for (const detail of details) {
+    const basicDetail = await database.scmProd.scm_order_details.findFirst({
+      where: {
+        reference_id: detail.supplier_reference_id,
+        reference_order_id: detail.order_id,
+      },
+    });
+
+    if (!basicDetail) {
+      console.log('!!! not found');
+      continue;
+    }
+
+    const orderDetail =
+      await database.scmOrderProd.procurement_order_details.findFirst({
+        where: {
+          reference_id: detail.supplier_reference_id,
+          procurement_orders: {
+            client_order_id: detail.order_id,
+          },
+        },
+      });
+
+    if (!orderDetail) {
+      console.log('!!! not found');
+      continue;
+    }
+    if (
+      Number(detail.final_qty) !== Number(orderDetail.final_qty) &&
+      Number(detail.final_qty) !== Number(basicDetail.delivery_qty)
+    ) {
+      console.log(
+        `!!! not match: ${detail.final_qty}, ${orderDetail.final_qty}, ${basicDetail.delivery_qty}, ${detail.actual_delivery_qty}`
+      );
+    }
+  }
+};
+
+run();
