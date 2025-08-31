b864982391516a7c65bf8fb71609b49c911b233c
aiden <aidenhsy@gmail.com>
2025-08-31 20:37:35 +0900

    updates

--- bbb-check-pricing.ts
+++ bbb-check-pricing.ts
@@ -8,11 +8,7 @@ const run = async () => {
       select: {
         price: true,
         supplier_reference_id: true,
-        supplier_orders: {
-          select: {
-            id: true,
-          },
-        },
+        order_id: true,
       },
       orderBy: {
         created_at: 'desc',
@@ -26,10 +22,45 @@ const run = async () => {
       where: {
         external_reference_id: detail.supplier_reference_id,
       },
+      select: {
+        sale_price: true,
+      },
     });
+    const orderDetail =
+      await database.scmOrderProd.procurement_order_details.findFirst({
+        where: {
+          reference_id: detail.supplier_reference_id,
+          procurement_orders: {
+            client_order_id: detail.order_id,
+          },
+        },
+        select: {
+          price: true,
+        },
+      });
     if (Number(pricing?.sale_price) !== Number(detail.price)) {
-      console.log(detail.supplier_reference_id, 'pricing mismatch');
-      continue;
+      console.log(
+        detail.supplier_reference_id,
+        'pricing mismatch',
+        'pricing price:',
+        pricing?.sale_price,
+        'detail price:',
+        detail.price,
+        'order detail price:',
+        orderDetail?.price
+      );
+    }
+    if (Number(orderDetail?.price) !== Number(detail.price)) {
+      console.log(
+        detail.supplier_reference_id,
+        'order detail mismatch',
+        'pricing price:',
+        pricing?.sale_price,
+        'order detail price:',
+        orderDetail?.price,
+        'detail price:',
+        detail.price
+      );
     }
   }
 };
