d7c054b67b61fc0d7d698216a8a5ae34e5535c4b
aiden <aidenhsy@gmail.com>
2025-08-31 19:57:10 +0900

    updates

---
+++ aaa-missing-receive-time.ts
@@ -0,0 +1,52 @@
+import { DatabaseService } from './database';
+
+const run = async () => {
+  const database = new DatabaseService();
+
+  const orders = await database.imProcurementProd.supplier_orders.findMany({
+    where: {
+      receive_time: null,
+      status: {
+        in: [4, 5],
+      },
+    },
+  });
+  for (const order of orders) {
+    const bOrder = await database.scmProd.scm_order_details.findFirst({
+      where: {
+        reference_order_id: order.id,
+      },
+      select: {
+        scm_order: {
+          select: {
+            arrival_time: true,
+          },
+        },
+      },
+    });
+
+    if (!bOrder || bOrder.scm_order.arrival_time === null) {
+      console.log(order.id, 'not found');
+      continue;
+    }
+    await database.imProcurementProd.supplier_orders.update({
+      where: {
+        id: order.id,
+      },
+      data: {
+        receive_time: bOrder.scm_order.arrival_time,
+      },
+    });
+
+    await database.scmOrderProd.procurement_orders.update({
+      where: {
+        client_order_id: order.id,
+      },
+      data: {
+        customer_receive_time: bOrder.scm_order.arrival_time,
+      },
+    });
+  }
+};
+
+run();
