dee973140ee9ca214086b1fbd1f204f4bf1811be
Aiden Yang <aidenhsy@gmail.com>
2025-09-01 16:31:50 +0900

    bug fix

--- bbb-check-qty.ts
+++ bbb-check-qty.ts
@@ -9,6 +9,7 @@ const run = async () => {
         created_at: 'desc',
       },
       select: {
+        id: true,
         final_qty: true,
         supplier_reference_id: true,
         order_id: true,
@@ -33,6 +34,29 @@ const run = async () => {
     }
 
     if (Number(order.final_qty) !== Number(detail.final_qty)) {
+      const scmBasic = await database.scmProd.scm_order_details.findFirst({
+        where: {
+          reference_id: detail.supplier_reference_id,
+          reference_order_id: detail.order_id,
+        },
+      });
+
+      await database.scmOrderProd.procurement_order_details.update({
+        where: {
+          id: order.id,
+        },
+        data: {
+          final_qty: scmBasic?.delivery_qty,
+        },
+      });
+      await database.imProcurementProd.supplier_order_details.update({
+        where: {
+          id: detail.id,
+        },
+        data: {
+          final_qty: scmBasic?.delivery_qty,
+        },
+      });
       console.log(detail.supplier_reference_id, 'qty mismatch');
     }
   }
