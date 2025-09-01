aa5cd3c4f0e26ce39be3b761a24b922e88d2a8c2
Aiden Yang <aidenhsy@gmail.com>
2025-09-01 15:38:24 +0900

    bug fix

--- check-pricing.ts
+++ check-pricing.ts
@@ -3,7 +3,11 @@ import { DatabaseService } from './database';
 const run = async () => {
   const database = new DatabaseService();
   const supplierItems =
-    await database.imProcurementProd.supplier_items.findMany();
+    await database.imProcurementProd.supplier_items.findMany({
+      where: {
+        status: 1,
+      },
+    });
 
   for (const supplierItem of supplierItems) {
     const pricing = await database.scmPricingProd.scm_good_pricing.findFirst({
@@ -12,7 +16,7 @@ const run = async () => {
       },
     });
 
-    if (pricing?.sale_price !== supplierItem.price) {
+    if (Number(pricing?.sale_price) !== Number(supplierItem.price)) {
       console.log(supplierItem.supplier_reference_id, 'not equal');
     }
   }
