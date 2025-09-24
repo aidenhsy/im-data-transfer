063ebbda667d0ec06fc3467d149358f3177d1b50
aiden <aidenhsy@gmail.com>
2025-09-24 14:34:43 +0900

    added details

--- aaa-checkqty.ts
+++ aaa-checkqty.ts
@@ -10,16 +10,7 @@ const run = async () => {
   const details = await procurementDB.supplier_order_details.findMany({
     where: {
       order_id: {
-        in: [
-          'c19af095-9c8b-40ca-8d27-163ed032055a',
-          '3b4b8007-524a-41de-80cf-bae8fcfa950e',
-          '0c0c98ac-06c4-4a79-95b7-8fc1ef178506',
-          '00347f7a-a705-49ec-a250-1aacfddf4026',
-          '908245c9-1bda-4fed-80f1-8722cf11dd7d',
-          'cd4d21b1-0087-42cb-8813-fe70e7d05b30',
-          'c75a8f30-a0e9-4a58-b0c8-f0b77a66fa40',
-          '224fdb36-ca7a-4510-9874-4f549413abc3',
-        ],
+        in: ['122bdc04-f8d8-42ca-b1db-96d61691858f'],
       },
     },
   });
