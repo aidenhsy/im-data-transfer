a15cab34eb346d71cf7e8e1ae318f52b93d41cc8
aiden <aidenhsy@gmail.com>
2025-09-29 12:33:25 +0900

    bug fix

--- ledger/check-ledger.ts
+++ ledger/check-ledger.ts
@@ -3,48 +3,18 @@ import { DatabaseService } from '../database';
 const run = async () => {
   const database = new DatabaseService();
 
-  const ledger = await database.imAccountingProd.inventory_ledger.findMany({
-    select: {
-      biz_type_id: true,
-      total_value: true,
-      source_detail_id: true,
-    },
-    where: {
-      created_at: {
-        gte: new Date('2025-09-01T00:00:00.000Z'),
-      },
-    },
-  });
-
-  console.log(ledger.length);
-
-  const inIds = ledger
-    .filter((item) => item.biz_type_id === 1)
-    .map((item) => item.source_detail_id);
-  const outIds = ledger
-    .filter((item) => item.biz_type_id === 2)
-    .map((item) => item.source_detail_id);
-
-  const missingInIds =
-    await database.imProcurementProd.supplier_order_details.findMany({
-      where: {
-        id: {
-          notIn: inIds,
-        },
-      },
-    });
-
-  const missingOutIds =
-    await database.imProcurementProd.supplier_order_return_details.findMany({
-      where: {
-        id: {
-          notIn: outIds,
-        },
-      },
-    });
-
-  console.log('missingInIds', missingInIds);
-  console.log('missingOutIds', missingOutIds);
+  // const ledger = await database.imAccountingProd.inventory_ledger.findMany({
+  //   select: {
+  //     biz_type_id: true,
+  //     total_value: true,
+  //     source_detail_id: true,
+  //   },
+  //   where: {
+  //     created_at: {
+  //       gte: new Date('2025-09-01T00:00:00.000Z'),
+  //     },
+  //   },
+  // });
 
   // for (const item of ledger) {
   //   if (item.biz_type_id === 1) {
@@ -94,6 +64,45 @@ const run = async () => {
   //     }
   //   }
   // }
+
+  const inIds =
+    await database.imProcurementProd.supplier_order_details.findMany({
+      where: {
+        supplier_orders: {
+          status: 4,
+          receive_time: {
+            gte: new Date('2025-09-01T00:00:00.000Z'),
+          },
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+  console.log(inIds.length);
+  for (const inId of inIds) {
+    const ledger = await database.imAccountingProd.inventory_ledger.findFirst({
+      where: {
+        source_detail_id: inId.id,
+      },
+    });
+    if (!ledger) {
+      console.log(inId.id, 'no in ledger');
+      continue;
+    }
+  }
+
+  const outIds =
+    await database.imProcurementProd.supplier_order_return_details.findMany({
+      where: {
+        supplier_order_returns: {
+          status: 1,
+          updated_at: {
+            gte: new Date('2025-09-01T00:00:00.000Z'),
+          },
+        },
+      },
+    });
 };
 
 run();
