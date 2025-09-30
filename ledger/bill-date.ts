51bb6ca54a467b48b95642d3dd66f29befcccf9d
aiden <aidenhsy@gmail.com>
2025-09-30 09:38:45 +0900

    added other side info

---
+++ ledger/bill-date.ts
@@ -0,0 +1,35 @@
+import { DatabaseService } from '../database';
+
+const run = async () => {
+  const database = new DatabaseService();
+
+  const bills = await database.imAccountingProd.inventory_ledger_bill.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  for (const bill of bills) {
+    const ledger = await database.imAccountingProd.inventory_ledger.findFirst({
+      where: {
+        bill_id: bill.id,
+      },
+      select: {
+        created_at: true,
+        updated_at: true,
+      },
+    });
+
+    await database.imAccountingProd.inventory_ledger_bill.update({
+      where: {
+        id: bill.id,
+      },
+      data: {
+        created_at: ledger?.created_at,
+        updated_at: ledger?.updated_at,
+      },
+    });
+  }
+};
+
+run();
