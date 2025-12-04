a196393fb224ba332b77a029e48abaae42c2218f
aiden <aidenhsy@gmail.com>
2025-12-04 21:51:24 +0800

    update submit to not delete

--- 0000000000-resubmit-order.ts
+++ 0000000000-resubmit-order.ts
@@ -9,7 +9,7 @@ const run = async () => {
       status: 50,
       type: 9,
       created_at: {
-        gte: new Date('2025-11-12 00:00:00'),
+        gte: new Date('2025-12-04 00:00:00'),
       },
     },
     select: {
