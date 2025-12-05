320d9f5053f2a1a3203bede1316a298807152f1b
aiden <aidenhsy@gmail.com>
2025-12-05 14:58:06 +0800

    updates

---
+++ ddddd-pricing.ts
@@ -0,0 +1,48 @@
+import { DatabaseService } from './database';
+
+const run = async () => {
+  const database = new DatabaseService();
+
+  const orders = await database.imProcurementProd.supplier_orders.findMany({
+    where: {
+      order_date: '2025-12-05',
+      type: 3,
+    },
+    include: {
+      supplier_order_details: true,
+    },
+  });
+
+  for (const order of orders) {
+    for (const detail of order.supplier_order_details) {
+      if (detail.supplier_reference_id?.startsWith('20251204-')) {
+        const newReferenceId = detail.supplier_reference_id.replace(
+          '20251204-',
+          '20251205-'
+        );
+
+        const pricing =
+          await database.scmPricingProd.scm_good_pricing.findFirst({
+            where: {
+              external_reference_id: newReferenceId,
+            },
+          });
+
+        if (!pricing) {
+          console.log(newReferenceId, 'no pricing');
+          continue;
+        }
+        console.log(newReferenceId, 'updating');
+        await database.imProcurementProd.supplier_order_details.update({
+          where: { id: detail.id },
+          data: {
+            price: Number(pricing.sale_price),
+            supplier_reference_id: newReferenceId,
+          },
+        });
+      }
+    }
+  }
+};
+
+run();
