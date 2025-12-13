bfbc58435861ffe737c67f7ee59fb02aaa9de95d
aiden <aidenhsy@gmail.com>
2025-12-13 22:05:35 +0900

    added

--- 0000000000-resubmit-order.ts
+++ 0000000000-resubmit-order.ts
@@ -9,7 +9,7 @@ const run = async () => {
       status: 50,
       type: 9,
       created_at: {
-        gte: new Date('2025-12-12 00:00:00'),
+        gte: new Date('2025-12-13 00:00:00'),
       },
     },
     select: {
