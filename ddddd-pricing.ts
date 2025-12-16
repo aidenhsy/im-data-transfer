223229eabc66813b8d4efcd672a9780cf44bc2f6
aiden <aidenhsy@gmail.com>
2025-12-16 22:11:15 +0900

    bug fix

--- ddddd-pricing.ts
+++ ddddd-pricing.ts
@@ -5,7 +5,7 @@ const run = async () => {
 
   const orders = await database.imProcurementProd.supplier_orders.findMany({
     where: {
-      order_date: '2025-12-15',
+      order_date: '2025-12-16',
       type: {
         in: [3, 9],
       },
@@ -17,10 +17,10 @@ const run = async () => {
 
   for (const order of orders) {
     for (const detail of order.supplier_order_details) {
-      if (detail.supplier_reference_id?.startsWith('20251214-')) {
+      if (detail.supplier_reference_id?.startsWith('20251215-')) {
         const newReferenceId = detail.supplier_reference_id.replace(
-          '20251214-',
-          '20251215-'
+          '20251215-',
+          '20251216-'
         );
 
         const pricing =
